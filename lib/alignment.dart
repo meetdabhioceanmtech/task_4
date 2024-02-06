59ca7e3b8a2331459181fa41a8fa4113959274e2
Meet <meetdabhi.oceanmtech@gmail.com>
2024-02-06 12:07:43 +0530

    basic done
    basic done

--- lib/alignment.dart
+++ lib/alignment.dart
@@ -11,83 +11,90 @@ class _AlignmentTask5State extends State<AlignmentTask5> {
   final List<Offset> _positions = [const Offset(0.0, 0.0), const Offset(100.0, 100.0)];
   final List<double> _imageScales = [1.0, 1.0];
   final List<double> _imageSizes = [100.0, 150.0];
-  final double alignmentThreshold = 5.0; // Adjust the threshold as needed
+  final int alignmentThreshold = 2; // Adjust the threshold as needed
   double _horizontalLineY = 0;
   double _verticalLineX = 0;
   bool isShow = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: AspectRatio(
-          aspectRatio: 1.0,
-          child: Stack(
-            children: [
-              Container(color: Colors.grey),
-              // CustomPaint(
-              //   painter: DottedLinePainter(_horizontalLineY, _verticalLineX),
-              // ),
-              Visibility(
-                visible: isShow,
-                child: CustomPaint(
-                  painter: DottedLinePainter(_horizontalLineY, _verticalLineX),
-                  size: const Size(double.infinity, double.infinity), // Set a large size
-                ),
-              ),
+      body: Container(
+        color: Colors.grey.withOpacity(0.3),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: AspectRatio(
+              aspectRatio: 1.0,
+              child: Stack(
+                children: [
+                  Container(color: Colors.white),
+                  // CustomPaint(
+                  //   painter: DottedLinePainter(_horizontalLineY, _verticalLineX),
+                  // ),
+                  Visibility(
+                    visible: isShow,
+                    child: CustomPaint(
+                      painter: DottedLinePainter(_horizontalLineY, _verticalLineX),
+                      size: const Size(double.infinity, double.infinity), // Set a large size
+                    ),
+                  ),
 
-              for (int i = 0; i < _positions.length; i++)
-                Positioned(
-                  left: _positions[i].dx,
-                  top: _positions[i].dy,
-                  child: GestureDetector(
-                    onScaleStart: (details) {
-                      _showAlignmentLines(i);
-                      _imageScales[i] = 1.0;
-                    },
-                    onScaleUpdate: (details) {
-                      _showAlignmentLines(i);
+                  for (int i = 0; i < _positions.length; i++)
+                    Positioned(
+                      left: _positions[i].dx,
+                      top: _positions[i].dy,
+                      child: GestureDetector(
+                        onScaleStart: (details) {
+                          _showAlignmentLines(i);
+                          _imageScales[i] = 1.0;
+                        },
+                        onScaleUpdate: (details) {
+                          _showAlignmentLines(i);
 
-                      if (details.pointerCount == 2) {
-                        double newSize = _imageSizes[i] * details.scale;
-                        double newScale = details.scale;
+                          if (details.pointerCount == 2) {
+                            double newSize = _imageSizes[i] * details.scale;
+                            double newScale = details.scale;
 
-                        if (newSize >= 100.0 && newSize <= 400.0) {
-                          setState(() {
-                            _imageScales[i] = newScale.clamp(1.0, 4.0);
-                            _imageSizes[i] = newSize;
-                          });
-                        }
-                      } else {
-                        setState(() {
-                          _positions[i] += details.focalPointDelta;
+                            if (newSize >= 100.0 && newSize <= 400.0) {
+                              setState(() {
+                                _imageScales[i] = newScale.clamp(1.0, 4.0);
+                                _imageSizes[i] = newSize;
+                              });
+                            }
+                          } else {
+                            setState(() {
+                              _positions[i] += details.focalPointDelta;
 
-                          double clampedX = _positions[i].dx;
-                          double clampedY = _positions[i].dy;
+                              double clampedX = _positions[i].dx;
+                              double clampedY = _positions[i].dy;
 
-                          _positions[i] = Offset(clampedX, clampedY);
-                        });
-                      }
-                    },
-                    onScaleEnd: (details) {
-                      _imageScales[i] = 1.0;
-                      // isShow = false;
-                      setState(() {});
-                    },
-                    child: Transform.scale(
-                      scale: _imageScales[i],
-                      child: SizedBox(
-                        height: _imageSizes[i],
-                        width: _imageSizes[i],
-                        child: Image.network(
-                          'https://img.freepik.com/free-photo/blue-wall-background_53876-88663.jpg?w=740&t=st=1706849199~exp=1706849799~hmac=8d50b1869dcaa518eaadb637f5de92cd0edc1e3ae59d1ee88d9377e258d1b91a',
+                              _positions[i] = Offset(clampedX, clampedY);
+                            });
+                          }
+                        },
+                        onScaleEnd: (details) {
+                          _imageScales[i] = 1.0;
+                          // isShow = false;
+                          setState(() {});
+                        },
+                        child: Transform.scale(
+                          scale: _imageScales[i],
+                          child: SizedBox(
+                            height: _imageSizes[i],
+                            width: _imageSizes[i],
+                            child: Image.network(
+                              'https://img.freepik.com/free-photo/blue-wall-background_53876-88663.jpg?w=740&t=st=1706849199~exp=1706849799~hmac=8d50b1869dcaa518eaadb637f5de92cd0edc1e3ae59d1ee88d9377e258d1b91a',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ),
-            ],
+                ],
+              ),
+            ),
           ),
         ),
       ),
@@ -96,19 +103,96 @@ class _AlignmentTask5State extends State<AlignmentTask5> {
 
   void _showAlignmentLines(int currentIndex) {
     isShow = true;
-    double currentX = _positions[currentIndex].dx + _imageSizes[currentIndex] / 2;
-    double currentY = _positions[currentIndex].dy + _imageSizes[currentIndex] / 2;
 
     _horizontalLineY = 0;
     _verticalLineX = 0;
 
     for (int i = 0; i < _positions.length; i++) {
       if (i != currentIndex) {
-        double targetX = _positions[i].dx + _imageSizes[i] / 2;
-        double targetY = _positions[i].dy + _imageSizes[i] / 2;
+        // Horizontal
+        double targetXcenter = _positions[i].dx + _imageSizes[i] / 2;
+        double targetXright = _positions[i].dx + _imageSizes[i];
+        double targetXleft = _positions[i].dx;
 
-        double distanceX = (targetX - currentX).abs();
-        double distanceY = (targetY - currentY).abs();
+        double currentXcenter = _positions[currentIndex].dx + _imageSizes[currentIndex] / 2;
+        double currentXbottom = _positions[currentIndex].dx + _imageSizes[currentIndex];
+        double currentXtop = _positions[currentIndex].dx;
+
+        if (targetXcenter.toInt() >= currentXcenter.toInt() - 2 &&
+            targetXcenter.toInt() <= currentXcenter.toInt() + 2) {
+          _verticalLineX = currentXcenter;
+        }
+        if (targetXright.toInt() >= currentXcenter.toInt() - 2 && targetXright.toInt() <= currentXcenter.toInt() + 2) {
+          _verticalLineX = currentXcenter;
+        }
+        if (targetXleft.toInt() >= currentXcenter.toInt() - 2 && targetXleft.toInt() <= currentXcenter.toInt() + 2) {
+          _verticalLineX = currentXcenter;
+        }
+        if (targetXcenter.toInt() >= currentXbottom.toInt() - 2 &&
+            targetXcenter.toInt() <= currentXbottom.toInt() + 2) {
+          _verticalLineX = currentXbottom;
+        }
+        if (targetXright.toInt() >= currentXbottom.toInt() - 2 && targetXright.toInt() <= currentXbottom.toInt() + 2) {
+          _verticalLineX = currentXbottom;
+        }
+        if (targetXleft.toInt() >= currentXbottom.toInt() - 2 && targetXleft.toInt() <= currentXbottom.toInt() + 2) {
+          _verticalLineX = currentXbottom;
+        }
+        if (targetXcenter.toInt() >= currentXtop.toInt() - 2 && targetXcenter.toInt() <= currentXtop.toInt() + 2) {
+          _verticalLineX = currentXtop;
+        }
+        if (targetXright.toInt() >= currentXtop.toInt() - 2 && targetXright.toInt() <= currentXtop.toInt() + 2) {
+          _verticalLineX = currentXtop;
+        }
+        if (targetXleft.toInt() >= currentXtop.toInt() - 2 && targetXleft.toInt() <= currentXtop.toInt() + 2) {
+          _verticalLineX = currentXtop;
+        }
+
+        // Horizontal
+        double targetYcenter = _positions[i].dy + _imageSizes[i] / 2;
+        double targetYbottom = _positions[i].dy + _imageSizes[i];
+        double targetYtop = _positions[i].dy;
+
+        double currentYcenter = _positions[currentIndex].dy + _imageSizes[currentIndex] / 2;
+        double currentYbottom = _positions[currentIndex].dy + _imageSizes[currentIndex];
+        double currentYtop = _positions[currentIndex].dy;
+
+        //TOP
+        if (targetYtop.toInt() >= currentYcenter.toInt() - 2 && targetYtop.toInt() <= currentYcenter.toInt() + 2) {
+          _horizontalLineY = currentYcenter;
+        }
+        if (targetYtop.toInt() >= currentYtop.toInt() - 2 && targetYtop.toInt() <= currentYtop.toInt() + 2) {
+          _horizontalLineY = currentYtop;
+        }
+        if (targetYtop.toInt() >= currentYbottom.toInt() - 2 && targetYtop.toInt() <= currentYbottom.toInt() + 2) {
+          _horizontalLineY = currentYbottom;
+        }
+
+        //Center
+        if (targetYcenter.toInt() >= currentYcenter.toInt() - 2 &&
+            targetYcenter.toInt() <= currentYcenter.toInt() + 2) {
+          _horizontalLineY = currentYcenter;
+        }
+        if (targetYcenter.toInt() >= currentYtop.toInt() - 2 && targetYcenter.toInt() <= currentYtop.toInt() + 2) {
+          _horizontalLineY = currentYtop;
+        }
+        if (targetYcenter.toInt() >= currentYbottom.toInt() - 2 &&
+            targetYcenter.toInt() <= currentYbottom.toInt() + 2) {
+          _horizontalLineY = currentYbottom;
+        }
+
+        //Bottom
+        if (targetYbottom.toInt() >= currentYcenter.toInt() - 2 &&
+            targetYbottom.toInt() <= currentYcenter.toInt() + 2) {
+          _horizontalLineY = currentYcenter;
+        }
+        if (targetYbottom.toInt() >= currentYtop.toInt() - 2 && targetYbottom.toInt() <= currentYtop.toInt() + 2) {
+          _horizontalLineY = currentYtop;
+        }
+        if (targetYbottom.toInt() >= currentYbottom.toInt() - 2 &&
+            targetYbottom.toInt() <= currentYbottom.toInt() + 2) {
+          _horizontalLineY = currentYbottom;
+        }
 
         // List<Offset> temPositions = [];
         // temPositions.addAll(_positions);
@@ -116,15 +200,15 @@ class _AlignmentTask5State extends State<AlignmentTask5> {
         // if (temPositions.any((element) => element.dy == _positions[currentIndex].dy)) {
         //   _verticalLineX = (currentY + targetY) / 2;
         // }
-        if (distanceX < alignmentThreshold) {
-          _verticalLineX = (currentY + targetY) / 2;
-          print(_verticalLineX);
-        }
+        // if (distanceX < alignmentThreshold) {
+        //   _verticalLineX = (currentY + targetY) / 2;
+        //   print(_verticalLineX);
+        // }
 
-        if (distanceY < alignmentThreshold) {
-          _horizontalLineY = (currentX + targetX) / 2;
-          print(_horizontalLineY);
-        }
+        // if (distanceY < alignmentThreshold) {
+        //   _horizontalLineY = (currentX + targetX) / 2;
+        //   print(_horizontalLineY);
+        // }
       }
     }
     setState(() {});
@@ -141,12 +225,12 @@ class DottedLinePainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     final Paint paint = Paint()
       ..color = Colors.red
-      ..strokeWidth = 5
+      ..strokeWidth = 2
       ..strokeCap = StrokeCap.round
       ..style = PaintingStyle.stroke;
 
     const double dashWidth = 5;
-    const double dashSpace = 5;
+    const double dashSpace = 7;
 
     if (horizontalLineY > 0) {
       double currentX = 0;
