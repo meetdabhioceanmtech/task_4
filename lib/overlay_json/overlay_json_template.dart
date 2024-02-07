1a1c8309a92622097d378a750cbedb11ec24e5ad
Meet <meetdabhi.oceanmtech@gmail.com>
2024-02-07 13:33:24 +0530

    done

--- lib/overlay_json/overlay_json_template.dart
+++ lib/overlay_json/overlay_json_template.dart
@@ -61,15 +61,9 @@ class _OverlayJsonTemplateState extends State<OverlayJsonTemplate> {
                 child: Stack(
                   fit: StackFit.expand,
                   children: [
-                    Image.network(
-                      'https://img.freepik.com/free-photo/blue-wall-background_53876-88663.jpg?w=740&t=st=1706849199~exp=1706849799~hmac=8d50b1869dcaa518eaadb637f5de92cd0edc1e3ae59d1ee88d9377e258d1b91a',
+                    Image.asset(
+                      'assets/image/bg.jpeg',
                       fit: BoxFit.cover,
-                      loadingBuilder: (context, child, loadingProgress) {
-                        if (loadingProgress == null) return child;
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      },
                     ),
                     PageView(
                       allowImplicitScrolling: true,
